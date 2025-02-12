926b0faf8269efa7d76f5e2997348e8d2119d621
AnanoMakharashvili <ananomakharashvili@gmail.com>
2025-02-12 21:14:47 +0200

    Added custom checkbox colors for dark mode

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const switchThem = document.getElementById("icon-moon");
 const textInput = document.getElementById("textInput");
 const switchWhiteThem = document.getElementById("icon-sun");
-console.log(textInput);
+const headerThemDark = document.getElementById("logo-dark-theme");
+const headerThemLight = document.getElementById("logo-light-theme");
+const checkBoxDark = document.getElementById("checkbox");
+console.log(checkBoxDark);
 
 switchThem.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.add("dark-mode");
   textInput.style.backgroundColor = "#21222C";
   textInput.style.color = "#E4E4EF";
   switchThem.style.display = "none";
   switchWhiteThem.style.display = "block";
+  headerThemDark.style.display = "block";
+  headerThemLight.style.display = "none";
+  checkBoxDark.style.accentColor = "#D3A0FA";
 });
 
+switchWhiteThem.addEventListener("click", () => {
+  document.body.classList.remove("dark-mode");
+  textInput.style.backgroundColor = "#F2F2F7";
+  textInput.style.color = "black";
+  switchThem.style.display = "block";
+  switchWhiteThem.style.display = "none";
+  headerThemDark.style.display = "none";
+  headerThemLight.style.display = "block";
+});
 const data = [
   { letter: "E", value: 40, percentage: 16.06 },
   { letter: "I", value: 29, percentage: 11.65 },
